f1a0b7439980405384f13aeceb9f54b3fdf2fc5c
Rasoul Ghasemi <54258585+rasoulgh917@users.noreply.github.com>
2022-12-25 17:22:55 +0330

    Update exercise3.js

--- Chapter10/Exercise10.03/exercise3.js
+++ Chapter10/Exercise10.03/exercise3.js
@@ -1,46 +1,42 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     $.ajax({
         method: 'GET',
         dataType: 'json',
-        url: 'https://www.thesportsdb.com/api/v1/json/1/lookupevent.php', 
-data: {id: 441613},
+        url: 'https://www.thesportsdb.com/api/v1/json/2/eventslast.php',
+        data: {
+            id: 134862
+        },
 
-        success: function (data) {
-        
-            const game = data.events[0];
-  $("#game_date").html(game.dateEvent);
+        success: function(data) {
 
-      
+            const game = data.results[0];
+            $("#game_date").html(game.dateEvent);
 
             const homeScore = parseInt(game.intHomeScore);
-                     const awayScore = parseInt(game.intAwayScore);
+            const awayScore = parseInt(game.intAwayScore);
 
             const homeWinner = homeScore > awayScore;
             const tie = homeScore == awayScore;
 
-             $("#home_score").html(homeScore)
-                .addClass( (homeWinner || tie) ? "winning_score" : "")  
-            $("#away_score").html(awayScore)
-                .addClass( (!homeWinner || tie) ? "winning_score" : "");
+            $("#home_score").html(homeScore).addClass((homeWinner || tie) ? "winning_score" : "")
+            $("#away_score").html(awayScore).addClass((!homeWinner || tie) ? "winning_score" : "");
 
-           getTeamImage(game.idHomeTeam, "#home_img");
-            getTeamImage(game.idAwayTeam, "#away_img");
+			getTeamImage(game.idHomeTeam, "#home_img")
+			getTeamImage(game.idAwayTeam, "#away_img")
 
-            
             $("#game_table").show();
         }
     });
-    
+
 });
 
 function getTeamImage(teamId, imageId) {
-
-    $.getJSON('https://www.thesportsdb.com/api/v1/json/1/lookupteam.php', 
-        {id: teamId}, 
-        function(data) { 
-            const teamData = data.teams[0];   
-            $(imageId).attr("src", teamData.strTeamBanner);
-        }
-    );
+	
+	$.getJSON('https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php', {
+        l: "NBA"
+    }, function(data) {
+		let teamData = data.teams.find(team => team.idTeam == teamId);
+        $(imageId).attr("src", teamData.strTeamBanner);
+	});
 }
