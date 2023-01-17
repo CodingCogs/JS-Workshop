1d7f0a79e996cebf74b51364fe674063b077b87a
Rasoul Ghasemi <54258585+rasoulgh917@users.noreply.github.com>
2023-01-17 21:06:40 +0330

    Changed Exercise Project

--- Chapter12/Exercise12.04/ts_app/src/index.ts
+++ Chapter12/Exercise12.04/ts_app/src/index.ts
@@ -1,3 +1,19 @@
-export function hello(name: string): string {
-  return 'Hello ' + name;
-}
+import { subtract } from "./app";
+
+function init() {
+    const form = document.querySelector("form");
+    form?.addEventListener("submit", submitHandler);
+  }
+
+  function submitHandler(e: Event) {
+    e.preventDefault();
+    const num1 = document.querySelector("input[name='firstnumber']") as HTMLInputElement;
+    const num2 = document.querySelector("input[name='secondnumber']") as HTMLInputElement;
+    const result = subtract(Number(num1.value), Number(num2.value));
+    const resultElement = document.querySelector("p");
+    if (resultElement) {
+      resultElement.textContent = result.toString();
+    }
+  }
+
+  init();
